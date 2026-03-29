bfd49d911f91ff2f6331bee1efed4381e29e4fc9
Hamzat Victor <v.hamzat@alustudent.com>
2026-03-29 14:24:53 +0200

    feat: add Learn screen and integrate into navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:carolie_tracking_app/src/screens/home_screen.dart';
+import 'package:carolie_tracking_app/src/screens/learn_screen.dart';
 import 'package:carolie_tracking_app/src/screens/log_meal_screen.dart';
 import 'package:carolie_tracking_app/src/theme/app_theme.dart';
 import 'package:carolie_tracking_app/src/widgets/app_shell.dart';
@@ -47,6 +48,8 @@ class _CalorieTrackerFlowState extends State<CalorieTrackerFlow> {
           onBackPressed: () => _showScreen(AppScreen.home),
           onTabSelected: _showScreen,
         );
+      case AppScreen.learn:
+        return LearnScreen(onTabSelected: _showScreen);
     }
   }
 }
